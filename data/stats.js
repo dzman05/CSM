9fdaa9e81fbe4abf45f4f1e61af2a14376d73425
dzman05 <aichi.rimi@gmail.com>
2026-01-13 14:53:32 +0100

    Update stats for باتنة

--- data/stats.js
+++ data/stats.js
@@ -1,5 +1,5 @@
 const NATIONAL_STATS = [
-        {
+    {
         "code": "05",
         "name": "باتنة",
         "students": 2,
@@ -10,13 +10,13 @@ const NATIONAL_STATS = [
             "الثالثة ثانوي": 1,
             "جامعي": 1
         },
-        "requests_total": 1,
+        "requests_total": 2,
         "requests_male": 0,
-        "requests_female": 0,
+        "requests_female": 1,
         "requested_distinct_courses": 0,
         "teachers_count": 2,
         "agreements_count": 1,
         "most_active_course": "حرفي مكرر لزيت الزيتون - عصر و تكرير تقليدي للزيتون",
-        "last_updated": "2026-01-13 14:35:12"
+        "last_updated": "2026-01-13 14:53:31"
     }
 ];
