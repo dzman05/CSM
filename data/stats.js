3fce56fb5f4dffe14d7e7a13b02968870cf55a59
dzman05 <aichi.rimi@gmail.com>
2026-01-13 14:09:16 +0100

    Add files via upload

---
+++ data/stats.js
@@ -0,0 +1,94 @@
+const NATIONAL_STATS = [
+    {
+        "code": "16",
+        "name": "الجزائر",
+        "revenue": 2500000,
+        "students": 530,
+        "courses": 14,
+        "last_updated": "2026-01-12 12:00:00"
+    },
+    {
+        "code": "31",
+        "name": "وهران",
+        "revenue": 1800000,
+        "students": 320,
+        "courses": 8,
+        "last_updated": "2026-01-12 12:00:00"
+    },
+    {
+        "code": "19",
+        "name": "سطيف",
+        "revenue": 1200000,
+        "students": 210,
+        "courses": 5,
+        "last_updated": "2026-01-12 12:00:00"
+    },
+    {
+        "code": "06",
+        "name": "بجاية",
+        "revenue": 950000,
+        "students": 180,
+        "courses": 4,
+        "last_updated": "2026-01-12 12:00:00"
+    },
+    {
+        "code": "30",
+        "name": "ورقلة",
+        "revenue": 1500000,
+        "students": 250,
+        "courses": 6,
+        "last_updated": "2026-01-12 12:00:00"
+    },
+    {
+        "code": "25",
+        "name": "قسنطينة",
+        "revenue": 1650000,
+        "students": 300,
+        "courses": 9,
+        "last_updated": "2026-01-12 12:00:00"
+    },
+    {
+        "code": "13",
+        "name": "تلمسان",
+        "revenue": 1100000,
+        "students": 190,
+        "courses": 4,
+        "last_updated": "2026-01-12 12:00:00"
+    },
+    {
+        "code": "07",
+        "name": "بسكرة",
+        "revenue": 900000,
+        "students": 160,
+        "courses": 3,
+        "last_updated": "2026-01-12 12:00:00"
+    },
+    {
+        "code": "10",
+        "name": "البويرة",
+        "revenue": 600000,
+        "students": 110,
+        "courses": 2,
+        "last_updated": "2026-01-12 12:00:00"
+    },
+    {
+        "code": "05",
+        "name": "باتنة",
+        "students": 2,
+        "courses": 2,
+        "students_male": 2,
+        "students_female": 0,
+        "students_edu_levels": {
+            "الثالثة ثانوي": 1,
+            "جامعي": 1
+        },
+        "requests_total": 3,
+        "requests_male": 1,
+        "requests_female": 1,
+        "requested_distinct_courses": 0,
+        "teachers_count": 2,
+        "agreements_count": 1,
+        "most_active_course": "حرفي مكرر لزيت الزيتون - عصر و تكرير تقليدي للزيتون",
+        "last_updated": "2026-01-13 10:43:04"
+    }
+];
