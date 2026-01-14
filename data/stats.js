9baff3f2257b75be14b83b4b457541637979f38a
dzman05 <aichi.rimi@gmail.com>
2026-01-14 11:13:38 +0100

    Add files via upload

--- data/stats.js
+++ data/stats.js
@@ -4,30 +4,32 @@ const NATIONAL_STATS = [
         "name": "باتنة",
         "lat": 28.03,
         "lng": 1.66,
-        "students": 3,
-        "students_male": 3,
+        "students": 2,
+        "students_male": 2,
         "students_female": 0,
-        "teachers_count": 0,
-        "courses": 0,
-        "requests_total": 2,
+        "teachers_count": 1,
+        "courses": 2,
+        "requests_total": 4,
         "requests_male": 1,
-        "requests_female": 0,
-        "requests_pending": 0,
-        "agreements_count": 0,
-        "most_active_course": "لا يوجد",
-        "last_updated": "2026-01-14 10:04:12",
+        "requests_female": 2,
+        "requests_pending": 2,
+        "agreements_count": 1,
+        "most_active_course": "حرفي مكرر لزيت الزيتون - عصر و تكرير تقليدي للزيتون",
+        "last_updated": "2026-01-14 09:42:16",
         "students_edu_levels": {
             "الثالثة ثانوي": 1,
-            "ثانية ثانوي": 1,
             "جامعي": 1
         },
         "age_groups": {
             "15-18": 0,
-            "19-25": 1,
-            "26-35": 1,
-            "35+": 1
+            "19-25": 2,
+            "26-35": 0,
+            "35+": 0
         },
-        "training_types": {},
+        "training_types": {
+            "تحسين مستوى": 1,
+            "تكوين أولي": 1
+        },
         "performance": {
             "this_year_registrations": 2,
             "success_rate": 0,
