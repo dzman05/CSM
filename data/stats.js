fd553a49682fe255d726a5ff8a7b46267f96ab09
dzman05 <aichi.rimi@gmail.com>
2026-01-15 08:50:22 +0100

    Update stats for الجزائر

--- data/stats.js
+++ data/stats.js
@@ -36,5 +36,36 @@ const NATIONAL_STATS = [
             "success_rate": 0,
             "graduated_count": 0
         }
+    },
+    {
+        "code": "16",
+        "name": "الجزائر",
+        "lat": 28.03,
+        "lng": 1.66,
+        "students": 0,
+        "students_male": 0,
+        "students_female": 0,
+        "teachers_count": 0,
+        "courses": 0,
+        "requests_total": 1,
+        "requests_male": 1,
+        "requests_female": 0,
+        "requests_pending": 0,
+        "agreements_count": 0,
+        "most_active_course": "لا يوجد",
+        "last_updated": "2026-01-15 08:50:21",
+        "students_edu_levels": {},
+        "age_groups": {
+            "15-18": 0,
+            "19-25": 0,
+            "26-35": 0,
+            "35+": 0
+        },
+        "training_types": {},
+        "performance": {
+            "this_year_registrations": 0,
+            "success_rate": 0,
+            "graduated_count": 0
+        }
     }
 ];
